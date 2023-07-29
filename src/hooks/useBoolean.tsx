b43a2ac0f7d554edac230c823301145362a39500
James Bellomy <jay.bells12@gmail.com>
2023-07-29 05:31:22 -0700

    Document boolean hook

--- src/hooks/useBoolean.tsx
+++ src/hooks/useBoolean.tsx
@@ -1,13 +1,17 @@
-import { useState } from "react";
-
+import { useState } from 'react'
+/**
+ * Hook for setting/reading a boolean value
+ * @param {boolean} initial initial value
+ * @returns {array} tuple with state value and controller object
+ */
 export const useBoolean = (initial: boolean = false) => {
-  const [state, setState] = useState(initial);
+  const [state, setState] = useState(initial)
 
   const controller = {
     on: () => setState(true),
     off: () => setState(false),
     toggle: () => setState((s) => !s),
-  };
+  }
 
-  return [state, controller] as const;
-};
+  return [state, controller] as const
+}
