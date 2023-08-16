7333c046a2389d33592990be5da4211bb9e0f8f8
James Bellomy <jay.bells12@gmail.com>
2023-08-16 15:56:37 -0700

    Add barebones login functionality

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,47 @@
+import type { Awaitable, NextAuthOptions, User } from 'next-auth'
+import usersData from '../../../../data/users.json'
+import CredentialsProvider from 'next-auth/providers/credentials'
+
+export const options: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        username: { label: 'Username', type: 'text' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials, req) {
+        // Find User
+
+        // If no user found, throw error?
+
+        // If user found, compare passwords
+
+        // If passwords do not match, throw error?
+
+        // If passwords match:
+        // get role from user
+        // set role in session?
+        // set authed to true?
+
+        const user = usersData.find(
+          (user) =>
+            user.username.toLowerCase() === credentials?.username.toLowerCase()
+        )
+        console.log('user: ', user)
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null
+
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      },
+    }),
+  ],
+  pages: {
+    signIn: '/login',
+  },
+}
--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,65 +1,82 @@
-"use client";
+'use client'
 
-import Image from "next/image";
-import { Button } from "../Button";
-import { useState } from "react";
+import Image from 'next/image'
+// import { Button } from '../Button'
+import { FormEvent, useState } from 'react'
+// import type {
+//   GetServerSidePropsContext,
+//   InferGetServerSidePropsType,
+// } from 'next'
+// import { getCsrfToken } from 'next-auth/react'
+import { signIn } from 'next-auth/react'
 
-export type LoginProps = {};
+// Need to add labels
+export type LoginProps = {}
 
-export const Login = (props: any) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+export const Login = (props: LoginProps) => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const loginUser = (e: FormEvent) => {
+    e.preventDefault()
+    signIn('credentials', { username, password })
+  }
 
   return (
-    <div className={"w-4/5 mb-4"}>
+    <form
+      onSubmit={loginUser}
+      className={'w-4/5 mb-4'}
+    >
       <input
         className={
-          "w-full bg-transparent border-0 border-b border-lighttext px-0 py-2 placeholder:text-lighttext focus:shadow-none focus:ring-0 focus:border-lighttext"
+          'w-full bg-transparent border-0 border-b border-lighttext px-0 py-2 placeholder:text-lighttext focus:ring-0 focus:border-lighttext'
         }
-        type={"text"}
-        name={"email"}
-        placeholder={"Email Address"}
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
+        type={'text'}
+        name={'username'}
+        placeholder={'Username'}
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
       />
-      <div className={"relative mb-8"}>
+      <div className={'relative mb-8'}>
         <input
           className={
-            "w-full bg-transparent border-0 border-b border-lighttext px-0 py-2 placeholder:text-lighttext focus:shadow-none focus:ring-0 focus:border-lighttext"
+            'w-full bg-transparent border-0 border-b border-lighttext px-0 py-2 placeholder:text-lighttext focus:ring-0 focus:border-lighttext'
           }
-          type={"password"}
-          name={"password"}
-          placeholder={"Password"}
+          type={'password'}
+          name={'password'}
+          placeholder={'Password'}
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
         <Image
-          className={"absolute top-3 right-3"}
-          src={"/icons_misc_eye.svg"}
-          alt={"eye"}
+          className={'absolute top-3 right-3'}
+          src={'/icons_misc_eye.svg'}
+          alt={'eye'}
           width={20}
           height={18}
         />
       </div>
-      <Button
-        text={"Log-In"}
-        link={"/"}
-        className={"w-full mb-8"}
-      />
-      <input
+      <button
         className={
-          "appearance-none w-5 h-5 rounded-md text-accent bg-transparent  focus:ring-0 focus:ring-offset-0"
+          'block bg-accent rounded-full px-6 pt-2.5 pb-3.5 text-primary font-bold tracking-def text-center w-full mb-8'
         }
-        type={"checkbox"}
-        id={"remember"}
-        name={"remember"}
-      />
-      <label
-        className={"text-darkttext tracking-def align-middle ml-3"}
-        htmlFor={"remember"}
       >
+        Log-In
+      </button>
+      <label
+        className={'text-darkttext tracking-def align-middle'}
+        htmlFor={'remember'}
+      >
+        <input
+          className={
+            'appearance-none w-5 h-5 rounded-md text-accent bg-transparent  focus:ring-0 focus:ring-offset-0 mr-2'
+          }
+          type={'checkbox'}
+          id={'remember'}
+          name={'remember'}
+        />
         Remember me
       </label>
-    </div>
-  );
-};
+    </form>
+  )
+}
