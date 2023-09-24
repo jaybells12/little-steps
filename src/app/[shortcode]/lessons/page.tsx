afc25671a416f0426223c3c76af96cf2d3379e67
James Bellomy <jay.bells12@gmail.com>
2023-09-23 20:27:18 -0700

    Add temporary testing to be removed later

--- src/app/[shortcode]/lessons/page.tsx
+++ src/app/[shortcode]/lessons/page.tsx
@@ -1,5 +1,6 @@
 import dbConnect from '@/utils/dbConnect'
 import Lesson from '@/models/Lesson'
+import User from '@/models/User'
 
 /**
  * VIEW ALL LESSONS PAGE
@@ -16,16 +17,37 @@ const testFn = async () => {
   return results
 }
 
+const testUserFn = async () => {
+  await dbConnect()
+  const newUser = await User.create({})
+}
+
 export default async function LessonsPage() {
-  async function Testing(data: FormData) {
+  async function Testing(formData: FormData) {
     'use server'
-    console.log(data)
+    // const { name, email, password, role } = data
+    // const user = {}
+    // console.log(formData.entries())
+    // for (const pair of formData.entries()) {
+    //   console.log(`${pair[0]}, ${pair[1]}`);
+    // }
+    const user = Object.fromEntries(formData.entries())
+    const [first, last] = user.name.split(' ')
+    user.name = {
+      first,
+      last,
+    }
+    console.log(user)
+    await dbConnect()
+    const newUser = await User.create(user)
+    console.log(newUser)
   }
   return (
     <form action={Testing}>
       <input name='name' />
       <input name='email' />
       <input name='password' />
+      <input name='role' />
       <button>Click Me</button>
     </form>
   )
