dd3041b2bde543620d4b17112829686e4479f086
James <jay.bells12@gmail.com>
2023-09-28 19:19:27 -0700

    Add edit form map

---
+++ src/components/Form/edit-form-map.ts
@@ -0,0 +1,30 @@
+export default {
+  // Classroom also has a curriculum field, where and how is curriculum added to classroom? from lessons?
+  classroom: {
+    name: 'text',
+    guide: 'select',
+    students: 'checkbox',
+  },
+  // When an admin is creating a new user, they should have access to setting the school code for the user
+  user: {
+    name: 'text',
+    email: 'email',
+    password: 'password',
+    role: 'select', // Role selection is permission based
+  },
+  //  Do the primary lesson groups need to be an enum?
+  lesson: {
+    name: 'text',
+    primary: 'text',
+    secondary: 'text',
+  },
+  // The school has a hashcode field generated automatically. School has Employees, Clients, Classrooms fields, not needed on initial creation
+  school: {
+    name: 'text'
+  },
+  student: {
+    name: 'text',
+    dob: 'date',
+    classroom: 'select',
+  },
+}
