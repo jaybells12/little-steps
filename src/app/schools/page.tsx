8918cb59637188f296bf80f43a05e0f626287b27
James <jay.bells12@gmail.com>
2023-10-12 06:53:50 -0700

    Rename variable

--- src/app/schools/page.tsx
+++ src/app/schools/page.tsx
@@ -10,10 +10,11 @@ import { getSchools } from './actions'
 import Link from 'next/link'
 import SchoolCard from '@/components/SchoolCard'
 
+//Async is how next js handles using this server action. Is it ok?
 export default async function SchoolsPage() {
   const schools = await getSchools()
 
-  const schoolComponents = schools.map(({ _id, name, hashcode, image, employees: { directors } }, idx) => {
+  const schoolCardComponents = schools.map(({ _id, name, hashcode, image, employees: { directors } }, idx) => {
     return (
       <Link key={idx} href={`/schools/${_id}`}>
         <SchoolCard name={name} image={image} hash={hashcode} directors={directors} abridged />
@@ -23,10 +24,10 @@ export default async function SchoolsPage() {
 
   return (
     <>
-      <Link href={'/home'} style={{ border: '2px solid black', padding: '0.5rem' }}>
+      <Link href={'/home'} className={'border border-black p-2 w-24 h-24 shrink-0'}>
         Home
       </Link>
-      <div className={'flex mx-28'}>{schoolComponents}</div>
+      {schoolCardComponents}
     </>
   )
 }
