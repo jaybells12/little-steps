195cfaafb9404b622dd7981e12530d39dc4939da
James <jay.bells12@gmail.com>
2023-10-16 07:27:05 -0700

    Setup School page by id

--- src/app/schools/[id]/page.tsx
+++ src/app/schools/[id]/page.tsx
@@ -1,7 +1,21 @@
 /**
  * VIEW A SCHOOL PAGE
+ * 1. Nav Bar
+ * 2. Side Menu
+ * 3. User Card, each card is link
+ * 4. Organizer Shell
  */
+import 'server-only'
 
-export default function SchoolPage() {
-  return <p>THIS IS A SCHOOL PAGE</p>
+import { getSchoolById } from '../actions'
+import School from '@/components/School'
+
+export const revalidate = 3600
+
+export default async function SchoolPage({ params }: { params: { id: string } }) {
+  const school = await getSchoolById(params.id)
+
+  const { name, hashcode, image, employees, clients, classrooms } = school
+
+  return <School name={name} hash={hashcode} imgSrc={image} employees={employees} clients={clients} classrooms={classrooms} />
 }
